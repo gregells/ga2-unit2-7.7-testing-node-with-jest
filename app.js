6170bb84768503398581bc97aaee401470c00fbe
Greg Ells <gregells@gmail.com>
2025-02-13 15:44:45 -0500

    add routes for /users

--- app.js
+++ app.js
@@ -2,8 +2,26 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/users', (req, res) => {
+  const { name, email } = req.body;
+  res.json({ name, email });
+});
+
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  res.json({ id, name, email });
+});
+
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({ id });
 });
 
 const PORT = process.env.PORT || 3000;
